2ed4c57c0485ddb320fc7d0ae4bfe78ed3ece893
Priyanshu Kumar <kpriyanshu256@Priyanshus-MacBook-Air.local>
2023-10-31 19:56:59 -0400

    Src for Task A

---
+++ src/task1/train.py
@@ -0,0 +1,207 @@
+from datasets import Dataset, load_dataset
+import pandas as pd
+import evaluate
+import numpy as np
+import transformers
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, AutoTokenizer, set_seed
+import os
+from sklearn.model_selection import train_test_split
+from scipy.special import softmax
+import argparse
+import logging
+import torch.nn as nn
+# import bitsandbytes as bnb
+from transformers.trainer_pt_utils import get_parameter_names
+
+
+MAX_LEN = 512
+
+def preprocess_function(examples, **fn_kwargs):
+    return fn_kwargs['tokenizer'](examples["text"], truncation=True, max_length=MAX_LEN)
+
+
+def compute_metrics(eval_pred):
+    acc_metric = evaluate.load("accuracy")
+
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    
+    results = {}
+    results.update(acc_metric.compute(predictions=predictions, references = labels))
+    return results
+
+
+def fine_tune(train_dataset, valid_dataset, checkpoints_path, id2label, label2id, model):
+    # get tokenizer and model from huggingface
+    tokenizer = AutoTokenizer.from_pretrained(model)     # put your model here
+    model = AutoModelForSequenceClassification.from_pretrained(
+       model, num_labels=len(label2id), id2label=id2label, label2id=label2id    # put your model here
+    )
+    
+    # tokenize data for train/valid
+    tokenized_train_dataset = train_dataset.map(preprocess_function, batched=True, fn_kwargs={'tokenizer': tokenizer})
+    tokenized_valid_dataset = valid_dataset.map(preprocess_function, batched=True,  fn_kwargs={'tokenizer': tokenizer})
+    
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=MAX_LEN)
+
+    # create Trainer 
+    training_args = TrainingArguments(
+        output_dir=checkpoints_path,
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        report_to = "none",
+        gradient_accumulation_steps = 8,
+        fp16 = True,
+        gradient_checkpointing = True,
+        save_total_limit = 1,
+        max_steps = 10,
+    )
+    """
+    decay_parameters = get_parameter_names(model, [nn.LayerNorm])
+    decay_parameters = [name for name in decay_parameters if "bias" not in name]
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if n in decay_parameters],
+            "weight_decay": training_args.weight_decay,
+        },
+        {
+            "params": [p for n, p in model.named_parameters() if n not in decay_parameters],
+            "weight_decay": 0.0,
+        },
+    ]
+
+    optimizer_kwargs = {
+        "betas": (training_args.adam_beta1, training_args.adam_beta2),
+        "eps": training_args.adam_epsilon,
+    }
+    optimizer_kwargs["lr"] = training_args.learning_rate
+    adam_bnb_optim = bnb.optim.Adam8bit(
+        optimizer_grouped_parameters,
+        betas=(training_args.adam_beta1, training_args.adam_beta2),
+        eps=training_args.adam_epsilon,
+        lr=training_args.learning_rate,
+    )
+    """
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train_dataset,
+        eval_dataset=tokenized_valid_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        # optimizers=(adam_bnb_optim, None)
+    )
+
+    trainer.train()
+
+    # save best model
+    best_model_path = checkpoints_path+'/best/'
+    
+    if not os.path.exists(best_model_path):
+        os.makedirs(best_model_path)
+    
+
+    trainer.save_model(best_model_path)
+
+
+def test(test_dataset, model_path, id2label, label2id):
+    
+    # load tokenizer from saved model 
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    # load best model
+    model = AutoModelForSequenceClassification.from_pretrained(
+       model_path, num_labels=len(label2id), id2label=id2label, label2id=label2id
+    )
+            
+
+    tokenized_test_dataset = test_dataset.map(preprocess_function, batched=True,  fn_kwargs={'tokenizer': tokenizer})
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=MAX_LEN)
+    
+
+    # create Trainer
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+    # get logits from predictions and evaluate results using classification report
+    predictions = trainer.predict(tokenized_test_dataset)
+    prob_pred = softmax(predictions.predictions, axis=-1)
+    preds = np.argmax(predictions.predictions, axis=-1)
+    metric = evaluate.load("bstrai/classification_report")
+    results = metric.compute(predictions=preds, references=predictions.label_ids)
+    
+    # return dictionary of classification report
+    return results, preds
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", "-tr", required=True, help="Path to the train file.", type=str)
+    parser.add_argument("--subtask", "-sb", required=True, help="Subtask (A or B).", type=str, choices=['A', 'B'])
+    parser.add_argument("--model", "-m", required=True, help="Transformer to train and test", type=str)
+    parser.add_argument("--prediction_file_path", "-p", required=True, help="Path where to save the prediction file.", type=str)
+
+    args = parser.parse_args()
+
+    random_seed = 0
+    model =  args.model # For example 'xlm-roberta-base'
+    subtask =  args.subtask # For example 'A'
+    prediction_path = args.prediction_file_path # For example subtaskB_predictions.jsonl
+    
+
+    if subtask == 'A':
+        id2label = {0: "human", 1: "machine"}
+        label2id = {"human": 0, "machine": 1}
+    elif subtask == 'B':
+        id2label = {0: 'human', 1: 'chatGPT', 2: 'cohere', 3: 'davinci', 4: 'bloomz', 5: 'dolly'}
+        label2id = {'human': 0, 'chatGPT': 1,'cohere': 2, 'davinci': 3, 'bloomz': 4, 'dolly': 5}
+    else:
+        logging.error("Wrong subtask: {}. It should be A or B".format(train_path))
+        raise ValueError("Wrong subtask: {}. It should be A or B".format(train_path))
+
+    set_seed(random_seed)
+        
+    dataset = load_dataset(args.dataset_name)    
+    dataset = dataset.class_encode_column("label")
+    train_ds = dataset['train']
+    train_ds = train_ds.train_test_split(test_size=0.3, 
+                                         seed = random_seed, 
+                                         stratify_by_column='label')
+    
+    
+    
+    train_dataset = train_ds['train']
+    valid_dataset = train_ds['test']
+    
+    # train detector model
+    logging.info("Starting training.......")
+    fine_tune(train_dataset, valid_dataset,
+              f"subtask{subtask}/{random_seed}", id2label, label2id, model)
+
+
+    test_dataset = dataset['dev']
+    
+    # test detector model
+    results, predictions = test(test_dataset, f"subtask{subtask}/{random_seed}/best/", id2label, label2id)
+    logging.info(results)
+    
+    test_ids = []
+    for x in test_dataset:
+        test_ids.append(x["id"])
+
+    print(results)
+    predictions_df = pd.DataFrame({'id': test_ids, 'label': predictions})
+    predictions_df.to_json(prediction_path, lines=True, orient='records')
